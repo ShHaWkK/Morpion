da362e44249968c129c7d205e253691f17dcbe1d
ShHaWkK <alexandreuzan9@gmail.com>
2024-02-02 12:30:30 +0100

    print game board

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def initialiser_plateau():
     return [[" " for _ in range(3)] for _ in range(3)]
 
+def afficher_plateau(plateau):
+    for ligne in plateau:
+        print("|".join(ligne))
+        print("-" * 5)
