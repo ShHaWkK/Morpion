47075f4da1e4f1534ab6c69a99156a9b8f5a9b19
ShHaWkK <alexandreuzan9@gmail.com>
2024-02-04 14:47:26 +0100

    import PhotoImage

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.messagebox
 import random
 import tkinter.font as tkFont
+from tkinter import ttk, PhotoImage
 
 def initialiser_plateau():
     return [["" for _ in range(3)] for _ in range(3)]
